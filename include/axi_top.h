2b620af404a0923ecc8364fd500a5869f5518a32
Paul Justen <justen.paul99@gmail.com>
2024-03-19 17:57:16 +0000

    added fsm, reworked channel, adjusted axi_top, added tb structure

--- include/axi_top.h
+++ include/axi_top.h
@@ -5,8 +5,13 @@
 #include "axi_channel.h"
 #include "fsm_master.h"
 #include "fsm_slave.h"
+#include "fsm_master.h"
+#include "fsm_slave.h"
 
 SC_MODULE(axi_top){
+    sc_in_clk ACLK;
+    sc_in<bool> ARESETn;
+    sc_in<bool> TRIGGER;
     sc_in_clk ACLK;
     sc_in<bool> ARESETn;
     sc_in<bool> TRIGGER;
@@ -14,8 +19,12 @@ SC_MODULE(axi_top){
     fsm_master master;
     fsm_slave slave;
     axi_channel channel;
+    fsm_master master;
+    fsm_slave slave;
+    axi_channel channel;
 
     SC_CTOR(axi_top) : master("master"), slave("slave"), channel("channel") {
+        cout << "axi_top constructor" << endl;
         master.ACLK(ACLK);
         master.ARESETn(ARESETn);
         master.channel(channel);
